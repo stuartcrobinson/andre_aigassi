00b8305c635e068651e72cb5aecc2e564f9e4ac2
Stuart Robinson <stuartrobinson@Stuarts-MacBook-Pro.local>
2019-06-10 20:08:52 -0400

    model notes guidelines

--- cctf/cnn.py
+++ cctf/cnn.py
@@ -1,4 +1,6 @@
 """
+goal: beat https://inoliao.github.io/CoachAI/
+
 define and run CNN on cctf images (cctf = color-coded temporal filter)
 
 how?  read training data as frames.
@@ -7,8 +9,22 @@ resize frame
 
 create generator to pull the next frame and feed into model
 
-model:
+model (try fewer filters than vgg and alexnet.  see modelOutlines_alexnet_vgg16.py)
+        rules of thumb: https://www.reddit.com/r/MachineLearning/comments/3l5qu7/rules_of_thumb_for_cnn_architectures/
 
-cnn - 15 filters
-cnn - 15 filters
+Conv2D(16, (3, 3)
+Conv2D(16, (3, 3)
+MaxPooling2D((4, 4), strides=(2, 2)
+
+Conv2D(16, (3, 3)
+Conv2D(16, (3, 3)
+MaxPooling2D((4, 4), strides=(2, 2)
+
+Conv2D(16, (3, 3)
+Conv2D(16, (3, 3)
+MaxPooling2D((4, 4), strides=(2, 2)
+
+
+
+
 """
---
+++ cctf/modelOutlines_alexnet_vgg16.py
@@ -0,0 +1,66 @@
+"""
+rules of thumb: https://www.reddit.com/r/MachineLearning/comments/3l5qu7/rules_of_thumb_for_cnn_architectures/
+
+https://stats.stackexchange.com/questions/296027/choosing-filter-size-strides-etc-in-a-cnn
+https://datascience.stackexchange.com/questions/25097/what-is-the-difference-between-8-filters-twice-and-one-16-filters-in-convolution
+https://www.quora.com/Why-does-convolution-neutral-network-increment-the-number-of-filters-in-each-layer
+
+? http://cs231n.github.io/convolutional-networks/
+
+vgg16: https://github.com/keras-team/keras-applications/blob/master/keras_applications/vgg16.py
+
+Input(shape=input_shape)
+
+Conv2D(64, (3, 3)
+Conv2D(64, (3, 3)
+MaxPooling2D((2, 2), strides=(2, 2)
+
+Conv2D(128, (3, 3)
+Conv2D(128, (3, 3)
+MaxPooling2D((2, 2), strides=(2, 2)
+
+Conv2D(256, (3, 3),
+Conv2D(256, (3, 3),
+Conv2D(256, (3, 3),
+MaxPooling2D((2, 2), strides=(2, 2)
+
+Conv2D(512, (3, 3),
+Conv2D(512, (3, 3),
+Conv2D(512, (3, 3),
+MaxPooling2D((2, 2), strides=(2, 2)
+
+Flatten(name='flatten')(x)
+Dense(4096, activation='relu', name='fc1')(x)
+Dense(4096, activation='relu', name='fc2')(x)
+Dense(classes, activation='softmax', name='predictions')(x)
+
+alexnet: https://github.com/eweill/keras-deepcv/blob/master/models/classification/alexnet.py
+
+
+Conv2D(96, (11, 11), input_shape=img_shape,
+MaxPooling2D(pool_size=(2, 2)))
+
+Conv2D(256, (5, 5),
+MaxPooling2D(pool_size=(2, 2)))
+
+ZeroPadding2D((1, 1)))
+Conv2D(512, (3, 3),
+MaxPooling2D(pool_size=(2, 2)))
+
+ZeroPadding2D((1, 1)))
+Conv2D(1024, (3, 3),
+
+ZeroPadding2D((1, 1)))
+Conv2D(1024, (3, 3),
+MaxPooling2D(pool_size=(2, 2)))
+
+Flatten())
+Dense(3072))
+Dropout(0.5))
+
+Dense(4096))
+Dropout(0.5))
+
+Dense(n_classes))
+Activation('softmax'))
+"""
